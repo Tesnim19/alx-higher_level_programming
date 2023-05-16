df6088461685889103d55f31e02b637ea68da60a
Tesnim19 <tesnimabdi67@gmail.com>
2023-05-16 19:11:19 +0000

    show record

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
--- script that lists all records with score >= 10
-SELECT score,name 
-FROM second_talbe 
+-- Script that lists all records with a score >= 10 in the second_table of hbtn_0c_0
+SELECT score, name
+FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
